fb6c132465faaf0fce1b2df52a4c77cebde15dba
ArthurCFDev <artcostafranca@gmail.com>
2024-02-05 17:35:06 -0300

    2nd commit

---
+++ Arvore de Decisao para Jogar Tenis.py
@@ -0,0 +1,23 @@
+def decidir_jogar_tenis(aspecto, umidade, vento):
+    if aspecto == "Sol":
+        if umidade == "Normal":
+            return "S"
+        else:
+            return "N"
+    elif aspecto == "Nuvens":
+        return "S"
+    elif aspecto == "Chuva":
+        if vento == "Forte":
+            return "N"
+        else:
+            return "S"
+    else:
+        return "N"
+
+
+aspecto_input = input()
+umidade_input = input()
+vento_input = input()
+
+decisao = decidir_jogar_tenis(aspecto_input, umidade_input, vento_input)
+print(decisao)
